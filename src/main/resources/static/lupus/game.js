d9d62a55fa29f1ece36b7d70df580db016c1c2ff
l4-ky <lucamatu2018@gmail.com>
2025-02-04 07:06:47 +0100

    .

--- src/main/resources/static/lupus/game.js
+++ src/main/resources/static/lupus/game.js
@@ -9,10 +9,23 @@ fetch(URLPrefix+"canOpenWebsocket/"+gameName, {
     /* teoricamente non ho bisogno di controllare alcun parametro perchè il metodo del Controller risponde solo quando ottiene il permesso di far aprire le Websocket */
     socket=new WebSocket(URLPrefix+gameName+"/"+username);
     socket.addEventListener("open", (event) => {
-        console.log("websocket opened.")
+        console.log("websocket opened.");
     });
     socket.addEventListener("message", (event) => {
         console.log(event.data);
         //logica x messagi in input
+        //TO DO: ricavare messaggio;
+        let mess;
+        if (mess.code==200) {
+            addMessageToChat(mess.sender, mess.message);
+        } else if (mess.code=300) {
+            mess.message.forEach((player) => {
+                //TO DO: aggiungere le cards per i player
+            });
+        }
     });
 });
+
+function addMessageToChat(sender, message) {
+    //TO DO
+}
